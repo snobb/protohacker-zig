f58f9dd9a6b775f5d49274f74ede1cca8485b20c
Alex Kozadaev <akozadaev at yahoo com>
2023-06-14 19:08:29 +0100

    task00: first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# protohacker Zig sandbox
+
+This is the repo for tinkering with protohacker exercises.
+
+Please see for details: https://protohackers.com/problems
+
+## golang
+
+The initial protohacker implementations in go: https://github.com/snobb/protohacker
+
+## typescript
+
+I'm also working on completing the same challenge in TS, which can be found on: https://github.com/snobb/protohacker-ts
---
+++ task00/src/main.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const net = std.net;
+const debug = std.debug;
+const thread = std.Thread;
+
+pub fn main() !void {
+    var listener = net.StreamServer.init(net.StreamServer.Options{});
+    const port = 8080;
+    try listener.listen(try net.Address.parseIp4("0.0.0.0", port));
+    debug.print("listening on port {d}\n", .{port});
+
+    while (true) {
+        var conn = listener.accept() catch break;
+        _ = try thread.spawn(thread.SpawnConfig{}, handler, .{conn});
+    }
+    debug.print("shutting down\n", .{});
+}
+
+fn handler(conn: net.StreamServer.Connection) !void {
+    var buf: [10]u8 = undefined;
+
+    debug.print("{} connected\n", .{conn.address});
+
+    while (true) {
+        var n = conn.stream.read(&buf) catch break;
+        if (n == 0) break;
+        _ = conn.stream.write(buf[0..n]) catch break;
+    }
+
+    conn.stream.close();
+    debug.print("{} disconnected\n", .{conn.address});
+}
